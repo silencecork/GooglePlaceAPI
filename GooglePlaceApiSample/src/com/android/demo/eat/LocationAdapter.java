be178c311f495d7d54762c4faffa42e0fe787222
silencecork <silencecork@gmail.com>
2013-12-08 14:38:55 +0800

    sample

---
+++ GooglePlaceApiSample/src/com/android/demo/eat/LocationAdapter.java
@@ -0,0 +1,61 @@
+package com.android.demo.eat;
+
+import java.util.List;
+
+import com.silencecork.google.search.nearby.Place;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class LocationAdapter extends BaseAdapter {
+	
+	private List<Place> places;
+	private LayoutInflater inflater;
+	
+	public LocationAdapter(Context c, List<Place> p) {
+		places = p;
+		inflater = LayoutInflater.from(c);
+	}
+	
+	@Override
+	public int getCount() {
+		return (places != null) ? places.size() : 0;
+	}
+
+	@Override
+	public Object getItem(int pos) {
+		return (places != null) ? places.get(pos) : null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.list_item, null);
+		}
+		
+		Place place = (Place) getItem(position);
+		if (place == null) {
+			return null;
+		}
+		
+		TextView placeName = (TextView) convertView.findViewById(R.id.place_name);
+		TextView placeAddress = (TextView) convertView.findViewById(R.id.place_address);
+		TextView placeRating = (TextView) convertView.findViewById(R.id.rating);
+		
+		placeName.setText(place.getName());
+		placeAddress.setText(place.getVicinity());
+		placeRating.setText(String.valueOf(place.getRating()));
+		
+		return convertView;
+	}
+
+}
---
+++ GooglePlaceApiSample/src/com/android/demo/eat/Main.java
@@ -0,0 +1,58 @@
+package com.android.demo.eat;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.ListView;
+
+import com.silencecork.google.search.nearby.GooglePlaceApi;
+import com.silencecork.google.search.nearby.OnSearchResultListener;
+import com.silencecork.google.search.nearby.Place;
+
+public class Main extends Activity implements OnSearchResultListener {
+	
+	
+	private GooglePlaceApi place;
+	private LocationManager locationManager;
+	private LocationAdapter adapter;
+	private ListView listView;
+	private static final String API_KEY = "";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        if (TextUtils.isEmpty(API_KEY)) {
+        	throw new NullPointerException("You must assign API KEY");
+        }
+        
+        place = new GooglePlaceApi(API_KEY, this);
+        
+        place.search(24.989926, 121.545414, 2000, "food");
+        
+        listView = (ListView) findViewById(R.id.list);
+    }
+    
+    @Override
+    public void onDestroy() {
+    	super.onDestroy();
+    }
+    
+	@Override
+	public void onResult(List<Place> places) {
+		adapter = new LocationAdapter(Main.this, places);
+		listView.setAdapter(adapter);
+	}
+
+	@Override
+	public void onFail() {
+		
+	}
+}
