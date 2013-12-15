7b3844ccab2b1e51f2eaa2d4c805d760d156521f
silencecork <silencecork@gmail.com>
2013-12-15 15:54:22 +0800

    add simple example

---
+++ GooglePlaceSimpleDemo/src/com/example/googleplacesimpledemo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.googleplacesimpledemo;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.silencecork.google.search.nearby.GooglePlaceApi;
+import com.silencecork.google.search.nearby.OnSearchResultListener;
+import com.silencecork.google.search.nearby.Place;
+
+public class MainActivity extends Activity implements OnSearchResultListener {
+	
+	private GooglePlaceApi placeApi;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        placeApi = new GooglePlaceApi("", this);
+        
+        placeApi.search(24.989926, 121.545414, 2000, "food");
+    }
+    
+    @Override
+    public void onDestroy() { 
+    	super.onDestroy();
+    }
+
+	@Override
+	public void onResult(List<Place> places) {
+		StringBuilder builder = new StringBuilder();
+		for (Place place : places) {
+			builder.append(place.getName());
+			builder.append(" ");
+			builder.append(place.getRating());
+			builder.append("\n");
+		}
+		TextView text = (TextView) findViewById(R.id.result);
+		text.setText(builder.toString());
+	}
+
+	@Override
+	public void onFail() {
+		
+	}
+}
